5c53124a847a4861e3e8cecee8ee091f07cdb7c7
Drew Nollsch <drew.nollsch@gmail.com>
2022-01-21 22:53:11 +0100

    created App Bar

---
+++ src/components/Homepage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Homepage() {
+  return (
+    <div>
+      <h1>Welcome to Radio TNP</h1>
+      <p>Some more text goes here...</p>
+    </div>
+  )
+}
